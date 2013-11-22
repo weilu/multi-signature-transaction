2f70440190dbf4947c2406edc69ffe4e67a15652
Wei Lu <luwei.here@gmail.com>
2013-11-22 14:12:26 +0800

    initial commit

---
+++ multi_signature_transaction.rb
@@ -0,0 +1,98 @@
+require_relative 'bitcoin_rpc'
+
+class MultiSignatureTransaction
+  def initialize buyer_publick_key, seller_public_key, escrow_public_key
+    @buyer_publick_key = buyer_publick_key
+    @buyer_address = Bitcoin.pubkey_to_address @buyer_pubilc_key
+
+    @seller_public_key = seller_public_key
+    @seller_address = Bitcoin.pubkey_to_address @seller_pubilc_key
+
+    @escrow_public_key = escrow_public_key
+
+    @client = BitcoinRPC.new('http://username:password@127.0.0.1:18332')
+
+    multi_sig = @client.createmultisig(2, [buyer_publick_key, seller_public_key, escrow_public_key])
+    @multi_sig_address = multi_sig["address"]
+    @redeem_script = multi_sig["redeemScript"]
+  end
+
+  # done by buyer
+  # escrow needs to verify transaction sent to multi_sig_address
+  def create_and_send prev_tx_id, buyer_private_key, starting_amount, send_amount
+    tx = @client.createrawtransaction [{txid: prev_tx_id, vout: 1}], {@multi_sig_address => send_amount, @buyer_address => starting_amount - send_amount}
+    puts "#{Time.now} | raw tx: #{tx}"
+
+    signed_tx = @client.signrawtransaction tx, [], [buyer_private_key]
+    puts "#{Time.now} | signed tx: #{signed_tx}"
+    raise 'failed to sign tx' unless signed_tx['complete']
+
+    @funding_tx = @client.decoderawtransaction(signed_tx['hex'])
+
+    send_tx signed_tx['hex']
+  end
+
+  # done by escrow once funding transaction is verified
+  # to be signed by seller
+  def create_payment_tx
+    create_spend_tx @seller_address
+  end
+
+  # done by seller
+  # to be signed by escrow once buyer's feature wins
+  def seller_sign_off_payment_tx tx, seller_private_key
+    half_signed_tx = @client.signrawtransaction tx, [funding_tx_hash], [seller_private_key]
+    puts "#{Time.now} | half signed spend tx: #{half_signed_tx}"
+    half_signed_tx['hex']
+  end
+
+  # done by escrow
+  def sign_off_and_send_payment_tx tx, escrow_private_key
+    signed_tx = @client.signrawtransaction tx, [funding_tx_hash], [escrow_private_key]
+    puts "#{Time.now} | signed spend tx: #{signed_tx}"
+    raise 'failed to sign off tx' unless signed_tx['complete']
+
+    send_tx signed_tx['hex']
+  end
+
+  # done by escrow once funding transaction is verified
+  # to be signed by buyer
+  def create_refund_tx
+    create_spend_tx @buyer_address
+  end
+
+  private
+
+  def send_tx signed_tx_hex
+    tx_id = @client.sendrawtransaction signed_tx_hex
+    puts "#{Time.now} | sent tx_id: #{tx_id}\n\n"
+    return tx_id
+  rescue Exception => e
+    puts "#{Time.now} | error: #{e.message}\n\n"
+  end
+
+  def funding_tx_hash
+    funding_tx_hash = {
+      txid: @funding_tx['txid'],
+      vout: 0,
+      scriptPubKey: script_pubkey,
+      redeemScript: @redeem_script
+    }
+  end
+
+  def script_pubkey
+    script = Bitcoin::Script.from_string(vout_to_multi_sig_address['scriptPubKey']['asm'])
+    script.to_binary.unpack("H*")[0]
+  end
+
+  def create_spend_tx to_address
+    tx = @client.createrawtransaction [{txid: @funding_tx['txid'], vout: 0}], {to_address => vout_to_multi_sig_address['value']}
+    puts "#{Time.now} | unsigned spend tx: #{tx}"
+    tx
+  end
+
+  def vout_to_multi_sig_address
+    @funding_tx['vout'].detect{|o| o['scriptPubKey']['addresses'].include?(@multi_sig_address)}
+  end
+end
+
