8acab06d543fd3c18ce1eebbffb758d9bf471d72
Wei Lu <luwei.here@gmail.com>
2013-11-27 00:52:38 +0800

    fix sign_tx command line tool
    now able to sign a p2sh tx with a redeem script

--- bin/sign_tx.rb
+++ bin/sign_tx.rb
@@ -5,22 +5,24 @@ require 'bitcoin/ffi/openssl'
 require 'thor'
 
 class SignTx < Thor
-  desc "sign SPENDING_TX FUNDING_TX PRIVATE_KEY", "sign SPENDING_TX with PRIVATE_KEY"
+
+  desc "sign SPENDING_TX FUNDING_TX REDEEM_SCRIPT PRIVATE_KEY",
+    "sign SPENDING_TX with PRIVATE_KEY and REDEEM_SCRIPT for FUNDING_TX"
   option :testnet3, type: :boolean, aliases: 't'
-  def sign tx_hex, prev_tx_hex, private_key
+
+  def sign tx_hex, prev_tx_hex, redeem_script_hex, private_key
     Bitcoin.network = :testnet3 if options[:testnet3]
 
     tx = Bitcoin::Protocol::Tx.new tx_hex.htb
     prev_tx = Bitcoin::Protocol::Tx.new prev_tx_hex.htb
 
     key = Bitcoin.open_key Bitcoin::Key.from_base58(private_key).priv
-    sig = Bitcoin.sign_data(key, tx.signature_hash_for_input(0, prev_tx))
+    sig = Bitcoin.sign_data(key, tx.signature_hash_for_input(0, prev_tx, nil, nil, nil, redeem_script_hex.htb))
 
-    script_sig = tx.in[0].script_sig
-    tx.in[0].script_sig = Bitcoin::Script.to_signature_pubkey_script(sig, [key.public_key_hex].pack("H*"))
-
-    puts tx.to_payload.unpack("H*")[0]
+    tx.in[0].script_sig = Bitcoin::Script.to_multisig_script_sig(sig + "\x01")
+    puts tx.to_payload.unpack('H*')[0]
   end
+
 end
 
 SignTx.start(ARGV)
